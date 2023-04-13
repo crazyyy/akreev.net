b597aefbdfc8198e1d01fae410366d16edba5fc3
Vitalii A <crazyyy@gmail.com>
2023-04-13 09:56:33 +0300

    update WP from server

--- wp-content/plugins/freepik-scraper/python/scraper_main.py
+++ wp-content/plugins/freepik-scraper/python/scraper_main.py
@@ -1,22 +1,24 @@
 import os
-from PIL import Image
 import re
 import json
+import time
+from PIL import Image
 from scraper import save_to_json, get_collection_data
-# from "wp-content.plugins.freepik-scraper.python.scraper" import save_to_json, get_collection_data
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# Step 1
+# Initialize WebDriver
 driver = webdriver.Firefox()
 
-# Step 2
+# Open gallery page
+# ToDo: move argument to variable. Make variable load from DB
 driver.get("https://www.freepik.com/author/dmitryakreev/collections")
 
-# Step 3-4
+# Loop over pages of collections
 collection_data_list = []
 next_page_button = True
 while next_page_button:
+    # Find all collection items and extract data
     collection_blocks = driver.find_elements(By.CSS_SELECTOR, ".collections article.collection__item")
     for block in collection_blocks:
         collection_data = {}
@@ -26,7 +28,7 @@ while next_page_button:
         collection_size = int(re.sub("[^0-9]", "", collection_size_str))
         collection_data["collection_size"] = collection_size
 
-        # Step 6
+        # Search for previous scraped files and check size of collection
         file_path = f"data\json\{collection_id}.json"
         if os.path.isfile(file_path):
             with open(file_path, "r") as fp:
@@ -34,11 +36,11 @@ while next_page_button:
             if data["collection_size"] >= collection_size:
                 continue
 
-        # Step 7
+        # Get collection URL
         collection_url = block.get_attribute("data-share-url")
         collection_data["collection_url"] = collection_url
 
-        # Step 8
+        # Save screenshot and update preview link
         driver.execute_script("""var links = document.querySelectorAll('.collection__link, .image-wrapper');
                                   for (i = 0; i < links.length; i++) {
                                     links[i].style.borderRadius = '0';
@@ -48,15 +50,17 @@ while next_page_button:
         image_wrapper.screenshot(screenshot_filename)
         collection_data["collection_preview"] = screenshot_filename
 
-        # Step 9-11
+        # Save collection data to file and update with additional data if available
         collection_data = get_collection_data(collection_url, collection_data)
         save_to_json(collection_data, f"{collection_id}.json")
 
+    # Move to next page of collection items
     try:
         next_page_button = driver.find_element(By.CSS_SELECTOR, ".pagination__next")
         next_page_button.click()
+        time.sleep(2)  # Wait for new content to load
     except:
         next_page_button = None
 
-# Step 12
+# Close WebDriver
 print("All done")
