c10983b74d0448fe190d319c589be320f77e842b
Vitalii A <crazyyy@gmail.com>
2023-04-18 02:13:57 +0300

    added rest api endpoints for update postmeta for atachment images

--- wp-content/plugins/freepik-scraper/freepik-scraper.php
+++ wp-content/plugins/freepik-scraper/freepik-scraper.php
@@ -51,3 +51,51 @@ add_action( 'init', 'freepik_scraper_initialize' );
 
 // Enqueue plugin scripts and styles
 add_action( 'wp_enqueue_scripts', 'freepik_scraper_enqueue_scripts' );
+
+
+
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'wp/v3', '/postmeta', array(
+        'methods'  => 'POST',
+        'callback' => 'freepik_update_postmeta',
+        'args'     => array(
+            'post_id'    => array(
+                'required' => true,
+                'type'     => 'integer',
+                'sanitize_callback' => 'absint',
+            ),
+            'meta_key'   => array(
+                'required' => true,
+                'type'     => 'string',
+                'sanitize_callback' => 'sanitize_text_field',
+            ),
+            'meta_value' => array(
+                'required' => true,
+                'type'     => 'string',
+                'sanitize_callback' => 'sanitize_text_field',
+            ),
+        ),
+        'permission_callback' => function () {
+            return current_user_can( 'edit_posts' );
+        }
+    ) );
+} );
+
+function freepik_update_postmeta( WP_REST_Request $request ) {
+    $post_id = $request->get_param( 'post_id' );
+    $meta_key = $request->get_param( 'meta_key' );
+    $meta_value = $request->get_param( 'meta_value' );
+
+    if ( empty( $post_id ) || empty( $meta_key ) || empty( $meta_value ) ) {
+        return new WP_Error( 'invalid_param', 'Invalid parameters', array( 'status' => 400 ) );
+    }
+
+    // Add or update postmeta row
+    $result = update_post_meta( $post_id, $meta_key, $meta_value );
+
+    if ( $result ) {
+        return new WP_REST_Response( array( 'success' => true ) );
+    } else {
+        return new WP_Error( 'update_postmeta_failed', 'Failed to update postmeta', array( 'status' => 500 ) );
+    }
+}
