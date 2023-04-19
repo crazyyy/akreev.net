87d3708cea18e4d1be93b80ece1dba8011f179bc
Vitalii A <crazyyy@gmail.com>
2023-04-20 02:12:16 +0300

    theme moved to WP

--- wp-content/themes/wp-akreev/footer.php
+++ wp-content/themes/wp-akreev/footer.php
@@ -1,19 +1,35 @@
-    </div><!-- /.inner -->
-  </section><!-- /section -->
+          <section class="back-to-top">
+            <a href="/#"><span class="arrow">↑</span><span class="preserve-whitespace">Back to
+                Top</span></a>
+          </section>
+          <a class="back-to-top-fixed js-back-to-top back-to-top-fixed-with-panel" href="/#">
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 26 26"
+              style="enable-background:new 0 0 26 26;" xml:space="preserve" class="icon icon-back-to-top">
+              <g>
+                <path
+                  d="M13.8,1.3L21.6,9c0.1,0.1,0.1,0.3,0.2,0.4c0.1,0.1,0.1,0.3,0.1,0.4s0,0.3-0.1,0.4c-0.1,0.1-0.1,0.3-0.3,0.4
+                        c-0.1,0.1-0.2,0.2-0.4,0.3c-0.2,0.1-0.3,0.1-0.4,0.1c-0.1,0-0.3,0-0.4-0.1c-0.2-0.1-0.3-0.2-0.4-0.3L14.2,5l0,19.1
+                        c0,0.2-0.1,0.3-0.1,0.5c0,0.1-0.1,0.3-0.3,0.4c-0.1,0.1-0.2,0.2-0.4,0.3c-0.1,0.1-0.3,0.1-0.5,0.1c-0.1,0-0.3,0-0.4-0.1
+                        c-0.1-0.1-0.3-0.1-0.4-0.3c-0.1-0.1-0.2-0.2-0.3-0.4c-0.1-0.1-0.1-0.3-0.1-0.5l0-19.1l-5.7,5.7C6,10.8,5.8,10.9,5.7,11
+                        c-0.1,0.1-0.3,0.1-0.4,0.1c-0.2,0-0.3,0-0.4-0.1c-0.1-0.1-0.3-0.2-0.4-0.3c-0.1-0.1-0.1-0.2-0.2-0.4C4.1,10.2,4,10.1,4.1,9.9
+                        c0-0.1,0-0.3,0.1-0.4c0-0.1,0.1-0.3,0.3-0.4l7.7-7.8c0.1,0,0.2-0.1,0.2-0.1c0,0,0.1-0.1,0.2-0.1c0.1,0,0.2,0,0.2-0.1
+                        c0.1,0,0.1,0,0.2,0c0,0,0.1,0,0.2,0c0.1,0,0.2,0,0.2,0.1c0.1,0,0.1,0.1,0.2,0.1C13.7,1.2,13.8,1.2,13.8,1.3z">
+                </path>
+              </g>
+            </svg>
+          </a>
+          <footer class="site-footer">
+            <div class="footer-text">
+              &copy; <?php echo date("Y"); ?> Powered by <?php bloginfo('name'); ?>
+            </div>
+          </footer>
+          </main>
+          </div>
+          </div>
+          </div>
+          <?php wp_footer(); ?>
+          <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+          </body>
 
-</div><!-- /wrapper -->
-
-<footer role="contentinfo">
-  <div class="inner">
-
-    <p class="copyright">
-      &copy; <?php echo date("Y"); ?> Собственность <?php bloginfo('name'); ?>.
-    </p><!-- /copyright -->
-
-  </div><!-- /.inner -->
-</footer><!-- /footer -->
-
-    <?php wp_footer(); ?>
-
-</body>
-</html>
+          </html>
--- wp-content/themes/wp-akreev/header.php
+++ wp-content/themes/wp-akreev/header.php
@@ -1,49 +1,94 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
-<head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="//www.google-analytics.com/" rel="dns-prefetch">
-  <link href="//fonts.googleapis.com" rel="dns-prefetch">
-  <link href="//cdnjs.cloudflare.com" rel="dns-prefetch">
+    <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?>
+    </title>
 
-  <!-- icons -->
-  <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
+    <link href="//www.google-analytics.com/" rel="dns-prefetch">
+    <link href="//fonts.googleapis.com" rel="dns-prefetch">
+    <link href="//cdnjs.cloudflare.com" rel="dns-prefetch">
 
-  <!--[if lt IE 9]>
+    <!-- icons -->
+    <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
+
+    <!--[if lt IE 9]>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <!-- css + javascript -->
-  <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<!-- wrapper -->
-<div class="wrapper">
-  <header role="banner">
-    <div class="inner">
+    <!-- css + javascript -->
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/proto.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/index.css" type="text/css">
+    <!-- <link rel="stylesheet" href="css/merged.css" type="text/css"> -->
+  </head>
 
-      <div class="logo">
-        <?php if ( !is_front_page() && !is_home() ){ ?>
-          <a href="<?php echo home_url(); ?>">
-        <?php } ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="logo-img">
-        <?php if ( !is_front_page() && !is_home() ){ ?>
-          </a>
-        <?php } ?>
-      </div><!-- /logo -->
-
-      <nav class="nav" role="navigation">
-        <?php wpeHeadNav(); ?>
-      </nav><!-- /nav -->
-
-    </div><!-- /.inner -->
-  </header><!-- /header -->
-
-  <section role="main">
-    <div class="inner">
+  <body <?php body_class(); ?>>
+    <!-- <body class="link-transition disable-download"> -->
+    <div class="page-background-video page-background-video-with-panel">
+    </div>
+    <div class="js-responsive-nav">
+      <div class="responsive-nav has-social">
+        <div class="close-responsive-click-area js-close-responsive-nav">
+          <div class="close-responsive-button"></div>
+        </div>
+        <div class="nav-container">
+          <nav>
+            <div class="gallery-title"><a href="/work" class="active">PSD Mockups</a>
+            </div>
+            <div class="gallery-title"><a href="/jpg">3D Render assets</a></div>
+            <div class="page-title">
+              <a href="/contact">Contact</a>
+            </div>
+            <div class="social pf-nav-social">
+              <ul>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <div class="site-wrap cfix js-site-wrap">
+      <div class="site-container">
+        <div class="site-content e2e-site-content">
+          <header class="site-header">
+            <div class="logo-container">
+              <div class="logo-wrap">
+                <div class="logo e2e-site-logo-text logo-text">
+                  <?php if ( !is_front_page() && !is_home() ){ ?>
+                  <a class="preserve-whitespace" href="<?php echo home_url(); ?>">
+                    <?php } ?>
+                    <span class="preserve-whitespace">Akreev</span>
+                    <?php if ( !is_front_page() && !is_home() ){ ?>
+                  </a>
+                  <?php } ?>
+                </div>
+              </div>
+              <div class="hamburger-click-area js-hamburger">
+                <div class="hamburger">
+                  <i></i>
+                  <i></i>
+                  <i></i>
+                </div>
+              </div>
+            </div>
+            <div class="nav-container">
+              <nav>
+                <div class="gallery-title">
+                  <a href="/work" class="active">PSD Mockups</a>
+                </div>
+                <div class="gallery-title">
+                  <a href="/jpg">3D Render assets</a>
+                </div>
+                <div class="page-title">
+                  <a href="/contact">Contact</a>
+                </div>
+              </nav>
+            </div>
+          </header>
+          <main>
