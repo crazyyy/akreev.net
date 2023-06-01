c32b3a988a77293fc58b49c6e95af56fc665623c
Vitalii A <crazyyy@gmail.com>
2023-06-01 13:16:54 +0300

    modify runner to close all firefox instances after task done

--- run_freepik_scraper.sh
+++ run_freepik_scraper.sh
@@ -13,3 +13,20 @@ echo "----- $(date) -----" >> ~/public_html/freepik-scraper.log
 echo "Publish unpublished galleries" >> ~/public_html/freepik-scraper.log
 python ./wp-content/plugins/freepik-scraper/python/post_media_acf_2_wp.py >> ~/public_html/freepik-scraper.log
 
+echo "----- $(date) -----" >> ~/public_html/freepik-scraper.log
+echo "Kill All running firefox instances" >> ~/public_html/freepik-scraper.log
+
+# Find all running Firefox processes
+firefox_pids=$(pgrep -f firefox)
+
+# Check if any Firefox processes are running
+if [ -z "$firefox_pids" ]; then
+  echo "No running Firefox instances found."
+else
+  # Kill each Firefox process
+  for pid in $firefox_pids; do
+    echo "Killing Firefox process with PID: $pid"
+    kill $pid
+  done
+  echo "All Firefox instances have been killed."
+fi
